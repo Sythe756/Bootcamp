90df543f748fbdb8da6ccee3995993505793380d
Sythe756 <alphatainted300@gmail.com>
2023-08-02 10:03:42 +0400

    there khavinD i fixed it

--- week-4/Day-1/exercise/exerciseXP.sql
+++ week-4/Day-1/exercise/exerciseXP.sql
@@ -1,17 +1,23 @@
-CREATE TABLE customers (
-	id SERIAL PRIMARY KEY,
-	first_name VARCHAR (50),
-	last_name VARCHAR (50)
-);
+-- CREATE TABLE items (
+-- 	id SERIAL PRIMARY KEY,
+-- 	label VARCHAR (20),
+-- 	price INT
+-- );
+
+-- CREATE TABLE customers (
+-- 	id SERIAL PRIMARY KEY,
+-- 	first_name VARCHAR (50),
+-- 	last_name VARCHAR (50)
+-- );
 
 
-INSERT INTO customers (first_name , last_name)
-VALUES('greg','jones'),
-('sandra','jones')
+-- INSERT INTO customers (first_name , last_name)
+-- VALUES('greg','jones'),
+-- ('sandra','jones');
 
-SELECT * FROM items,customers
-SELECT * FROM items WHERE price > 80
-SELECT * FROM items WHERE price <= 300
-SELECT * FROM customers WHERE last_name = 'smith' -- no operator matches the given name  and arguments types. you might need to add explicit type casts
-SELECT * FROM customers WHERE last_name = 'jones'
-SELECT * FROM customers WHERE first_name != 'scott'
+-- SELECT * FROM items,customers;
+-- SELECT * FROM items WHERE price > 80;
+-- SELECT * FROM items WHERE price <= 300;
+-- SELECT * FROM customers WHERE last_name = 'smith'; -- no operator matches the given name  and arguments types. you might need to add explicit type casts
+-- SELECT * FROM customers WHERE last_name = 'jones';
+-- SELECT * FROM customers WHERE first_name != 'scott';
